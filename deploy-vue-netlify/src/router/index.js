c2cfc9c6eebd196d0f462b8df0a3a9bd7f1f4e1d
M-Verhoosel-EY <mikaela.verhoosel@au.ey.com>
2023-08-26 11:47:23 +1000

    routing

--- deploy-vue-netlify/src/router/index.js
+++ deploy-vue-netlify/src/router/index.js
@@ -8,6 +8,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      name: "",
+      component: WelcomeView,
+    },
+    {
       path: "/welcome",
       name: "welcome",
       component: WelcomeView,
